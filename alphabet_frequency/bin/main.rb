9254e1ca9415aa251d430ad6ef5723e35e0ef6c8
Chandan Kumar <chandanchaudharyrules@gmail.com>
2017-01-18 16:41:54 +0530

    Change exit condition to q/Q Bugfix for class file name for require_relative in vowel_replacement program

--- alphabet_frequency/bin/main.rb
+++ alphabet_frequency/bin/main.rb
@@ -3,7 +3,7 @@ require_relative '../lib/alphabet_frequency'
 def user_menu
 	puts
 	puts "Enter string"
-	puts "Press 'q' to quit"
+	puts "Press 'q/Q' and then enter key to quit"
 	puts
 end
 
@@ -11,7 +11,7 @@ af = AlphabetFrequency.new
 
 user_menu
 while line = $stdin.gets.chomp
-	exit if line == 'q'
+	exit if line == 'q' || line == 'Q'
 	puts "String: #{line}"
 	puts "Hash  : #{af.convert_string_to_frequency_hash(line)}"
 	user_menu
--- vowel_replacement/bin/main.rb
+++ vowel_replacement/bin/main.rb
@@ -1,16 +1,16 @@
-require_relative '../lib/vowel-replacement'
+require_relative '../lib/vowel_replacement'
 
 def user_menu
 	puts
 	puts "Enter string"
-	puts "Press 'q' to quit"
+	puts "Press 'q/Q' and then enter key to quit"
 	puts
 end
 
 vr = VowelReplacement.new
 user_menu
 while line = $stdin.gets.chomp
-	exit if line == 'q'
+	exit if line == 'q' || line == 'Q'
 	puts "String    : #{line}"
 	puts "New String: #{vr.replace_vowels(line, '*')}"
 	user_menu
