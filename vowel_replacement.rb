adee94f362e2eadee33599fb62f90d00a9c0b05d
Chandan Kumar <chandanchaudharyrules@gmail.com>
2017-01-18 15:29:48 +0530

    Create vowel_replacement.rb program to replace all the vowels in the input string with '*' using regex

---
+++ vowel_replacement.rb
@@ -0,0 +1,21 @@
+class VowelReplacement
+    def replace_vowels(string, char)
+        string.gsub(/a|A|e|E|i|I|o|O|u|U/, char)
+    end
+end
+
+def user_menu
+    puts
+    puts "Enter string"
+    puts "Press 'q' to quit"
+    puts
+end
+
+vr = VowelReplacement.new
+user_menu
+while line = $stdin.gets.chomp
+	exit if line == 'q'
+    puts "String    : #{line}"
+    puts "New String: #{vr.replace_vowels(line, '*')}"
+    user_menu
+end
