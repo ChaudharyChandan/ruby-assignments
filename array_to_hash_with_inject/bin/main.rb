7ba448ae98f7bc66340c94bd6c0c480d7deea7b3
Chandan Kumar <chandanchaudharyrules@gmail.com>
2017-01-20 14:01:00 +0530

    Change for loop to inject and change method names

--- array_to_hash_with_inject/bin/main.rb
+++ array_to_hash_with_inject/bin/main.rb
@@ -1,3 +1,3 @@
 require_relative '../lib/array'
 
-p ['abc','def',1234,234,'abcd','x','mnop',5,'zZzZ'].seperate_odd_even
+p ['abc','def',1234,234,'abcd','x','mnop',5,'zZzZ'].odd_even_group_hash
--- array_to_hash_with_inject/lib/array.rb
+++ array_to_hash_with_inject/lib/array.rb
@@ -1,14 +1,13 @@
 class Array
-  def get_length_wise_hash
-    hash = Hash.new { |h, k| h[k] = [] }
-    for element in self
+  def length_wise_hash
+    self.inject( Hash.new { |h, k| h[k] = [] } ) do |hash, element|
       hash[element.to_s.length] << element
+      hash
     end
-    hash
   end
 
-  def seperate_odd_even
-    get_length_wise_hash.inject({:odd => [], :even => []}) do |result, (key, value)|
+  def odd_even_group_hash
+    length_wise_hash.inject({:odd => [], :even => []}) do |result, (key, value)|
       if key.odd?
         result[:odd] << value
       else
