d13b71739880fabd3af2b05c3ef860b08b6626c7
Chandan Kumar <chandanchaudharyrules@gmail.com>
2017-03-20 18:49:45 +0530

    Create a method that returns the no of various lowercase, uppercase, digits and special characters used in the string with make use of range.

---
+++ characters_count_using_range/bin/main.rb
@@ -0,0 +1,3 @@
+require_relative '../lib/array'
+
+puts 'Ahvsu busefbuew 3e9qB  CKBue90 q{?}][sbfs\'kjdiBSKJBFSoqw hdoq38y82'.split('').group_character_count
---
+++ characters_count_using_range/lib/array.rb
@@ -0,0 +1,17 @@
+class Array
+  def group_character_count
+    inject( Hash.new(0)) do | hash, element |
+      case element
+      when '0'..'9'
+        hash[:digits] += 1;
+      when 'a'..'z'
+        hash[:lowercase] += 1;
+      when 'A'..'Z'
+        hash[:uppercase] += 1;
+      when '!'..'~'
+        hash[:special] += 1;
+      end
+      hash
+    end
+  end
+end
